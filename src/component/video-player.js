b0244251351aae61a9b046c748f9cfcd0b27b33e
Adrian Fabiszewski <adrian.fabiszewski@outlook.com>
2018-09-04 17:47:00 +0200

    Traveler can watch fixed YouTube video - unit test for VideoPlayer

--- src/component/video-player.js
+++ src/component/video-player.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Text, View, Dimensions, WebView } from 'react-native';
+import {
+  Text, View, Dimensions, WebView,
+} from 'react-native';
 
 export default class VideoPlayer extends Component {
   render() {
@@ -8,13 +10,18 @@ export default class VideoPlayer extends Component {
     return (
       <View style={{ flex: 1 }}>
         <View style={{ flex: 1, backgroundColor: '#F5FCFF', justifyContent: 'flex-end' }}>
-          <Text style={{ textAlign: 'center', bottom: 10, fontSize: 24 }}> Piloot Route </Text>
+          <Text
+            id="route-title"
+            style={{ textAlign: 'center', bottom: 10, fontSize: 24 }}
+          >
+            Piloot Route
+          </Text>
         </View>
         <View style={{ flex: 3, backgroundColor: '#808080', alignItems: 'center' }}>
           <WebView
+            id="route-video-player"
             style={{ height: 300, width }}
             source={{ uri: 'https://www.youtube.com/embed/eRQRTk9CLpo' }}
-            renderError={() => alert('Internet connection error')}
           />
         </View>
       </View>
